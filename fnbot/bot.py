986aea925b5f2ef67ee493887a3d75bdcf3c8563
HRTK92 <takuho0920@outlook.jp>
2021-02-21 03:58:30 +0000

    update

--- fnbot/bot.py
+++ fnbot/bot.py
@@ -22,10 +22,8 @@ from . import web
 class MyBot(commands.Bot):
 	def __init__(self, settings: BotSettings, loop: asyncio.AbstractEventLoop) -> None:
 		self.settings = settings
-        self.loop=loop
         device_auth_details = self.get_device_auth_details().get(self.settings.email, {})
         super().__init__(
-	        loop=self.loop,
 		    command_prefix='!',
 		    status=self.settings.status,
 		    platform=fortnitepy.Platform(self.settings.platform),
